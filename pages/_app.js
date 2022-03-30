4d857224064a21c631dd51c283bc6aac29702af4
GloriaBargoria <bargoriagloria@gmail.com>
2022-03-30 19:28:30 +0300

    the homepage and shop and API created in api folder for data fetching

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer'
+// import store from '../redux/store';
+// import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    // <Provider store={store}>
+      <div className="wrapper">
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    // </Provider>
+  )
 }
 
 export default MyApp
