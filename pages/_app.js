f82f6b54e3033cb4672637dbc2a776f47886e3c5
GloriaBargoria <bargoriagloria@gmail.com>
2022-03-30 20:22:56 +0300

    implementation of redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer'
-// import store from '../redux/store';
-// import { Provider } from 'react-redux';
+import store from '../redux/store';
+import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }) {
   return(
-    // <Provider store={store}>
+    <Provider store={store}>
       <div className="wrapper">
         <Navbar />
         <Component {...pageProps} />
         <Footer />
       </div>
-    // </Provider>
+    </Provider>
   )
 }
 
