8bfca8a92a3da415e25eced50cc8c18b70cceb14
GloriaBargoria <bargoriagloria@gmail.com>
2022-04-01 23:51:03 +0300

    state management in the slider

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0);
+    
   return (
     <div>Slider</div>
   )
