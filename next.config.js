8f4762b7452d1dd89e482c3d386d3cdf18fa6215
GloriaBargoria <bargoriagloria@gmail.com>
2022-04-01 22:38:04 +0300

    edits on the config file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['i.ibb.co'],
+//   },
+// }
+
+// module.exports = nextConfig
+
+module.exports = {
   images: {
     domains: ['i.ibb.co'],
   },
-}
-
-module.exports = nextConfig
+};
